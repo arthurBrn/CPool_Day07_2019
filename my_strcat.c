1226d8bc0793ec031f1d31b6e0635c4c47eb97d7
Baron <arthur.baron@epitech.eu>
2019-10-09 10:54:18 +0200

    09/10|10:53|Task 02 my_strcat.c completed. Go through string1, when reaching '\0' it 'prints' the src string

---
+++ my_strcat.c
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2019
+** my_strcat
+** File description:
+** Task 02
+*/
+
+char *my_strcat(char *dest, char const *src)
+{
+    int i = 0;
+    int j = 0;
+
+    while (dest[i] != '\0') {
+        i++;
+    }
+    while (src[j] != '\0') {
+        dest[i] = src[j];
+        i++;
+        j++;
+    }
+    return (dest);
+}
